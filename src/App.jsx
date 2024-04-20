db17c933c59c09f694031008294f19749124f9e8
Hiep (Roger) Nguyen <hiepnguyenduc2005@gmail.com>
2024-04-20 15:01:54 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const location = {
+    'lat': '37.7749',
+    'lon': '-122.4194'
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        console.log(import.meta.env.VITE_JOKE_API)
+        const response = await fetch(
+          `https://api.openweathermap.org/data/3.0/onecall?lat=${location.lat}&lon=${location.lon}&appid=${import.meta.env.VITE_JOKE_API}`)
+        const data = await response.json()
+        console.log(data)
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
 
   return (
     <>
